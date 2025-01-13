74fb6d8eb7a2e1fd6012de7124163df539b98f26
ajac-zero <ajcardoza2000@gmail.com>
2025-01-13 14:26:49 -0600

    add async scripts

---
+++ scripts/benchmarks/async/_async_langchain.py
@@ -0,0 +1,27 @@
+import os
+import langchain_openai
+import asyncio
+
+client = langchain_openai.ChatOpenAI(
+    api_key=os.getenv("ORPHEUS_API_KEY", "mock"),
+    base_url=os.getenv("ORPHEUS_BASE_URL", "http://localhost:8100/openai"),
+)
+
+messages = [
+    {"role": "system", "content": "You are a friendly bot"},
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "hello, whats in the image"},
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://www.pawlovetreats.com/cdn/shop/articles/pembroke-welsh-corgi-puppy_2000x.jpg?v=1628638716"
+                },
+            },
+        ],
+    },
+]
+model = "gpt-4o"
+
+asyncio.run(client.ainvoke(input=messages, model=model))
---
+++ scripts/benchmarks/async/_async_orpheus.py
@@ -0,0 +1,28 @@
+import os
+import orpheus
+import asyncio
+
+client = orpheus.AsyncOrpheus(
+    api_key=os.getenv("ORPHEUS_API_KEY", "mock"),
+    base_url=os.getenv("ORPHEUS_BASE_URL", "http://localhost:8100/openai"),
+)
+
+messages = [
+    {"role": "system", "content": "You are a friendly bot"},
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "hello, whats in the image"},
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://www.pawlovetreats.com/cdn/shop/articles/pembroke-welsh-corgi-puppy_2000x.jpg?v=1628638716"
+                },
+            },
+        ],
+    },
+]
+model = "gpt-4o"
+
+
+asyncio.run(client.chat.completions.create(messages=messages, model=model))
