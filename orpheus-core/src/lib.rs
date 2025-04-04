071c7acd89e00d2969cf8c5823fe512c56db40c6
ajac-zero <ajcardoza2000@gmail.com>
2025-04-04 13:28:30 -0600

    rename nonblocking to concurrent

--- orpheus-core/src/lib.rs
+++ orpheus-core/src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code, clippy::too_many_arguments)]
 
 mod blocking;
+mod concurrent;
 mod constants;
 mod models;
-mod nonblocking;
 mod utils;
 
 use pyo3::prelude::*;
@@ -11,7 +11,7 @@ use pyo3::prelude::*;
 #[pymodule]
 fn orpheus_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<blocking::OrpheusCore>()?;
-    m.add_class::<nonblocking::AsyncOrpheusCore>()?;
+    m.add_class::<concurrent::AsyncOrpheusCore>()?;
     m.add_class::<models::chat::message::Message>()?;
     m.add_class::<models::chat::ChatCompletion>()?;
     m.add_class::<models::embed::EmbeddingResponse>()?;
