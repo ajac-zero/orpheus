95cf92baf2ffa663d38cd320626a9d5cec1e5fe6
ajac-zero <ajcardoza2000@gmail.com>
2025-06-26 20:13:24 +0900

    clean up leftover pyo3 code

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,10 @@
 
 mod client;
 mod constants;
-mod exceptions;
+mod error;
 pub mod models;
 
 pub use client::{AsyncOrpheus, Orpheus};
 
-pub type Error = exceptions::OrpheusError;
+pub type Error = error::OrpheusError;
 pub type Result<T, E = Error> = core::result::Result<T, E>;
-
-// #[pymodule]
-// fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
-//     m.add_class::<models::chat::Message>()?;
-//     m.add_class::<models::chat::ChatCompletion>()?;
-//     m.add_class::<models::chat::ToolCall>()?;
-//     m.add_class::<models::chat::Part>()?;
-//     Ok(())
-// }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,29 +1,2 @@
 pub mod chat;
 pub mod completion;
-
-// use pyo3::prelude::*;
-// use pythonize::{depythonize, pythonize};
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-
-/// This object accepts arbitrary python dictionaries,
-/// as long as the can be serialized to a JSON string.
-/// It is basically to be used as a wildcard for unknown dicts.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ArbitraryDict(Value);
-
-// impl<'py> FromPyObject<'py> for ArbitraryDict {
-//     fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-//         depythonize::<Value>(ob).map(Self).map_err(|e| e.into())
-//     }
-// }
-
-// impl<'py> IntoPyObject<'py> for ArbitraryDict {
-//     type Target = PyAny;
-//     type Output = Bound<'py, Self::Target>;
-//     type Error = PyErr;
-
-//     fn into_pyobject(self, py: Python<'py>) -> PyResult<Self::Output> {
-//         pythonize(py, &self.0).map_err(|e| e.into())
-//     }
-// }
